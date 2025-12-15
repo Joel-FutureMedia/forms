e84bc6314cbd0ed69784686fd99afda55975ccfd
Joel-FutureMedia <joel.kalimbwe@futuremedia.com.na>
2025-12-15 11:04:35 +0200

    Change API proxy target to production URL

--- setupProxy.js
+++ setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8383',
+      target: 'https://clientform.simplyfound.com.na',
       changeOrigin: true,
       secure: true,
       logLevel: 'debug',
