a7cc048ae63f2d347e29dd4dbe2bb120b4085f19
Joel-FutureMedia <joel.kalimbwe@futuremedia.com.na>
2025-12-15 11:05:07 +0200

    Change API base URL to production endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8383/api';
+const API_BASE_URL = 'https://clientform.simplyfound.com.na/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
