41b172501f88185d5a6bbcfc91e296b23d8bb18d
Joel-FutureMedia <joel.kalimbwe@futuremedia.com.na>
2026-02-12 11:03:10 +0200

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const API_BASE_URL = 'https://clientform.simplyfound.com.na/api';
-const API_BASE_URL = 'http://localhost:8383/api';
+const API_BASE_URL = 'https://clientform.simplyfound.com.na/api';
+//const API_BASE_URL = 'http://localhost:8383/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
